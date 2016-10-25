8741299ec71e24a372939d42eee2df251284ccb0
vcline <vishi.goel@gmail.com>
2016-10-24 19:22:27 -0500

    initial function creation and call

---
+++ callFindMean.R
@@ -0,0 +1,3 @@
+#call findMean function
+vec<-c(1,2,3,4,5)
+findmean(vec)
---
+++ findMean.R
@@ -0,0 +1,8 @@
+#find the mean of the vector
+findmean<-function(meanvector){
+    vectormean<-mean(meanvector)
+    vectormean
+}
+
+
+
